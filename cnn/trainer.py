e33c0400eb4bdeaf3d8ecad001891c782fb0b2ac
Paolo Fermin <paolofermin@vt.edu>
2021-04-14 22:05:45 -0400

    load image folder

--- cnn/trainer.py
+++ cnn/trainer.py
@@ -1,7 +1,22 @@
 from torch_cnn import Net
+import torch
 from torchvision import datasets
+import os
 
 net = Net()
 print(net)
 
-train_images = datasets.ImageFolder()
+trainset = datasets.ImageFolder(os.path.join('..', 'Train'))
+print(trainset)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
