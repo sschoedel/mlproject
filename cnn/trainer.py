bc31eec531bb5088eb3fb4a60735d252550f97e2
Paolo-Fermin <paolofermin@vt.edu>
2021-04-19 13:36:10 -0400

    complete training script

--- cnn/trainer.py
+++ cnn/trainer.py
@@ -1,22 +1,66 @@
 from torch_cnn import Net
 import torch
-from torchvision import datasets
+from torchvision import datasets, transforms
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformations import Rescale, RandomCrop
+
+batch_size = 4
+
+transform = transforms.Compose([
+    # you can add other transformations in this list
+    transforms.ToTensor(),
+    transforms.CenterCrop(100)
+    #Rescale(120) 
+    #RandomCrop(100)
+])
 
 net = Net()
 print(net)
 
-trainset = datasets.ImageFolder(os.path.join('..', 'Train'))
+trainset = datasets.ImageFolder(os.path.join('..', 'Train'), transform=transform)
 print(trainset)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True, num_workers=0)
 
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+def imshow(batch):
+    if batch.ndim == 4:
+        for img in batch:         
+            img = img / 2 + 0.5     # unnormalize
+            npimg = img.numpy()
+            plt.imshow(npimg)
+            plt.show()
 
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
