aa1e720f621d5745a18f4fabc522f7bff7f58316
Paolo-Fermin <paolofermin@vt.edu>
2021-04-21 14:11:55 -0400

    add comments explaining layer dimensions

--- cnn/torch_cnn.py
+++ cnn/torch_cnn.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# use https://madebyollin.github.io/convnet-calculator/ to calculate size of cnn layers
+
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5) # output: 6x96x96
+        self.pool = nn.MaxPool2d(2, 2)  # output: 6x48x48
+        self.conv2 = nn.Conv2d(6, 16, 5) # output: 16x44x44
+        # note that there is another maxpooling layer here with output 16x22x22
         self.fc1 = nn.Linear(7744, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 11)
