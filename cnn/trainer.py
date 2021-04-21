6aa728f8db9d44dafb6b5f88a95a4d9ea6f3a589
Paolo-Fermin <paolofermin@vt.edu>
2021-04-21 18:34:29 -0400

    add testing, better parameter inputs, gpu

--- cnn/trainer.py
+++ cnn/trainer.py
@@ -7,10 +7,21 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+
 from transformations import Rescale, RandomCrop
 
-batch_size = 4
+# set constants
+WORKERS = 0
+EPOCHS = 2
 
+# train on GPU, if available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+print(device)
+
+batch_size = 4
 transform = transforms.Compose([
     # you can add other transformations in this list
     transforms.ToTensor(),
@@ -22,22 +33,31 @@ transform = transforms.Compose([
 net = Net()
 print(net)
 
-trainset = datasets.ImageFolder(os.path.join('..', 'Train'), transform=transform)
+trainset = datasets.ImageFolder(os.path.join(os.getcwd(), '..', 'Train'), transform=transform)
 print(trainset)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=0)
+                                          shuffle=True, num_workers=WORKERS)
 
-import torch.optim as optim
+testset = datasets.ImageFolder(os.path.join(os.getcwd(), '..', 'Test'), transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                          shuffle=True, num_workers=WORKERS)
 
+# set loss and optimizer functions
+# use crossentropyloss for multi-class classification
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(2):  # loop over the dataset multiple times
+PRINT_INTERVAL = 1000
+
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
+        # replace the line above with this line if training on a gpu:
+        #inputs, labels = data.to(device)
+
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -50,9 +70,27 @@ for epoch in range(2):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % PRINT_INTERVAL == PRINT_INTERVAL-1:    # print every 1000 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+                  (epoch + 1, i + 1, running_loss / PRINT_INTERVAL))
             running_loss = 0.0
 
 print('Finished Training')
+
+# save model in the same folder as this script
+model_path = os.path.join(os.getcwd(), 'trained_cnn')
+torch.save(net, model_path)
+print(f'Saving model as {model_path}')
+
+# test the model
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the test images: {100*correct/total}')
