d15b6fdcfcd2c7eee1839025a095ae3d76e33fdc
Paolo Fermin <paolofermin@vt.edu>
2021-04-21 20:43:59 -0400

    add batchnorm and more conv layers

--- cnn/torch_cnn.py
+++ cnn/torch_cnn.py
@@ -7,19 +7,24 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5) # output: 6x96x96
+        # note that this pooling layer follows every conv layer
         self.pool = nn.MaxPool2d(2, 2)  # output: 6x48x48
+
+        self.conv1 = nn.Conv2d(3, 6, 3) # output: 6x96x96
+        self.bn1 = nn.BatchNorm2d(6)    # normalize
         self.conv2 = nn.Conv2d(6, 16, 5) # output: 16x44x44
-        # note that there is another maxpooling layer here with output 16x22x22
-        self.fc1 = nn.Linear(7744, 120)
+        self.conv3 = nn.Conv2d(16, 32, 5) 
+        # note that there is another maxpooling layer 
+        self.fc1 = nn.Linear(2592, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 11)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         # first argument should be batch size, and -1 will let pytorch figure out other dimensions
-        x = x.view(4, -1)
+        x = x.view(-1, 2592)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
